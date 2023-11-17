ff756b315d9aa99b784befb89a1c88561df5d462
Cesar Martinez Torres <20808@udla.fundacion.mx>
2023-11-17 08:19:22 -0600

    Tournament

---
+++ FourWheel Tournament/Tournament/controllers/4wheel/build/release/4wheel.d
@@ -0,0 +1,6 @@
+build/release/4wheel.o: 4wheel.c \
+ D:\Webots\Webots/include/controller/c/webots/distance_sensor.h \
+ D:\Webots\Webots/include/controller/c/webots/types.h \
+ D:\Webots\Webots/include/controller/c/webots/motor.h \
+ D:\Webots\Webots/include/controller/c/webots/robot.h \
+ D:\Webots\Webots/include/controller/c/webots/nodes.h
